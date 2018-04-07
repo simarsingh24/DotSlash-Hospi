efc6145a3315035c9f970abd591bd0b0074a48c8
shivaraju <shivaraju1357@gmail.com>
2018-04-07 17:26:40 +0530

    Profile activity

--- app/src/main/java/com/example/harsimar/hospi/MainActivity.java
+++ app/src/main/java/com/example/harsimar/hospi/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.harsimar.hospi;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +8,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String PREF_NAME = "DotSlash-Hospi";
-    public static final String IS_LOGIN = "is_logged_in";
-    int PRIVATE_MODE = 0;
-
-    private SharedPreferences mPref;
-    static SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPref = getApplicationContext().getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-        mEditor = mPref.edit();
-
-        String isLoggedIn = mPref.getString(IS_LOGIN, null);
-
-        if (isLoggedIn == null) {
-            //Toast.makeText(this,"going in",Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(this, Login_Activity.class));
-            finish();
-        } else {
-
-        }
         Button butt =  (Button) findViewById(R.id.but);
-        butt.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
-            }
-        });
+//        butt.setOnClickListener(new Button.OnClickListener(){
+//            public void onClick(View v){
+//                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+//            }
+//        });
     }
 
 
--- app/src/main/java/com/example/harsimar/hospi/ProfileActivity.java
+++ app/src/main/java/com/example/harsimar/hospi/ProfileActivity.java
@@ -1,17 +1,85 @@
 package com.example.harsimar.hospi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by shivaraju on 7/4/18.
  */
 
 public class ProfileActivity extends AppCompatActivity {
+
+    DatabaseReference databaseData;
+    Button submitgoInside;
+
+    EditText name;
+    EditText aadhar;
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
+
+        name = (EditText) findViewById(R.id.e1);
+        aadhar = (EditText) findViewById(R.id.e2);
+
+        submitgoInside = findViewById(R.id.but2);
+
+        databaseData = FirebaseDatabase.getInstance().getReference("Aadhar Number");
+
+        submitgoInside.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean q = false;
+                q = addData();
+
+                if (q) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(ProfileActivity.this, "Enter all details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    public boolean addData(){
+
+        String aadharData= aadhar.getText().toString().trim();
+        Intent i2 = new Intent(this,MainActivity.class);
+        i2.putExtra("aadhar",aadharData);
+
+        String nm = name.getText().toString().trim();
+        if (!TextUtils.isEmpty(nm)){
+
+            String id1=databaseData.push().getKey();
+            Data msg = new Data(id1,nm);
+            databaseData.child(aadharData).child("name").setValue(nm);
+
+        }else{
+            Toast.makeText(this, "enter name", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+
+    public static void aData(String aadhar, String  name){
+
     }
 }
